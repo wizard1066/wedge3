de9b6913ba96c45777179c7b86a8f72c491a4974
wizard1066 <mark.lucking@gmail.com>
2022-09-11 10:46:45 +0200

    Initial Commit

---
+++ wedge3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wedge3
+//
+//  Created by localuser on 11.09.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wedge3/wedge3App.swift
@@ -0,0 +1,17 @@
+//
+//  wedge3App.swift
+//  wedge3
+//
+//  Created by localuser on 11.09.22.
+//
+
+import SwiftUI
+
+@main
+struct wedge3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
